edd0af0a0083dfa84d00a8e061839bb42d9377ca
James Tobey <jtobey@ssicorp.socialsolutions.com>
2018-09-20 00:40:48 -0500

    new changes to API

--- API.js
+++ API.js
@@ -1,11 +1,22 @@
-const APIkey= "AIzaSyD70EcFlRlgIbc1ARNvns5CszqjaUyQzVI"
-var Address=$('#address').val()
-$.address.parameter('address', encodeURIComponent(address));
-$("#address-image").html("<img class='img-responsive img-thumbnail' src='https://maps.googleapis.com/maps/api/staticmap?size=600x200&maptype=roadmap&markers=" + encodeURIComponent(address) + "' alt='" + address + "' title='" + address + "' />");
+const APIkey = "AIzaSyD70EcFlRlgIbc1ARNvns5CszqjaUyQzVI"
 
-const searchRep = gif => {
-    $.get("https://www.googleapis.com/civicinfo/v2/representatives?key="+APIkey+"&address="+Address).then(response => {
-        for (let i = 0; i < response.departures.all.length; i++) {
+$(document).ready(function () {
+    $(document).on('click', ".submit", function (event) {
+        let Address = $(".address").val().trim();
+        let AddressClean = Address.replace(/\s/g, '')
+        let City = $(".city").val().trim();
+        let State = $(".state").val().trim();
+        let Country = $(".country").val().trim();
+        let AddressLookup = AddressClean
+        console.log(AddressLookup)
+        /*$.address.parameter('address', encodeURIComponent(address));*/
+        /*$(".address-image").html("<img class='img-responsive img-thumbnail' src='https://maps.googleapis.com/maps/api/staticmap?size=600x200&maptype=roadmap&markers=" + encodeURIComponent(address) + "' alt='" + address + "' title='" + address + "' />");*/
+
+        const searchRep = address => {
+            $.get("https://www.googleapis.com/civicinfo/v2/representatives?key=" + APIkey + "&address=" + AddressLookup).then(response => {
+            for ( let i=1; i<response.officials.length;i++)
+            console.log(`${response.offices[i].name}`+" "+`${response.officials[i].name}`)
+        /*for (let i = 0; i < response.departures.all.length; i++) {
 
             let destinationName = `${response.departures.all[i].destination_name}`;
             let trainName = `${response.departures.all[i].operator_name}`;
@@ -31,5 +42,9 @@ const searchRep = gif => {
         }
 
     })
-}
-searchTrain()
+}*/})
+        }
+        /*searchTrain()*/
+        searchRep()
+    })
+})
