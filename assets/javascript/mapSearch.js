0081d8a46bb0b102c5dbefdd9adc95cc7663c11f
Wesley Kruger <wesleykruger@Wesleys-Device.local>
2018-09-26 21:23:03 -0500

    organized javascript and html files

--- assets/javascript/mapSearch.js
+++ assets/javascript/mapSearch.js
@@ -1,4 +1,67 @@
-$(document).ready(function () {
+    //This is the section to trigger the function to initiate our Google APIs
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+    }
+    function initMultiple() {
+        initMap();
+        initAutocomplete();
+    }
+    // This example displays an address form, using the autocomplete feature
+    // of the Google Places API to help users fill in the information.
+
+    // This example requires the Places library. Include the libraries=places
+    // parameter when you first load the API. For example:
+    //<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYQHKMzQBNqWcLUvLKJNrSdXqZ90wOA88&libraries=places">
+
+    var placeSearch, autocomplete;
+    var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+    };
+
+    function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            { types: ['geocode'] });
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete.addListener('place_changed', fillInAddress);
+    }
+
+    function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+    }
+
+
+
+$(document).ready(function() {
     $(".searchMap").on("click", function () {
         let streetAddress = $(".streetAddress");
         let cityAddress = $(".cityAddress");
@@ -124,7 +187,7 @@ $(document).ready(function () {
             "Fri": "Friday",
             "Sat": "Saturday",
             "Sun": "Sunday"
-          };
-          return dayDict[day];
-}
+        };
+        return dayDict[day];
+    }
 });
