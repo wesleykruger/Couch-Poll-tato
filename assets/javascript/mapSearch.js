73cb47a238835cbf9759c833bc668dcd8ca88654
Kruges <wesleykruger@gmail.com>
2018-09-28 23:11:04 -0500

    fixed map errors

--- assets/javascript/mapSearch.js
+++ assets/javascript/mapSearch.js
@@ -45,7 +45,6 @@ function fillInAddress() {
     var place = autocomplete.getPlace();
 
     for (var component in componentForm) {
-        console.log(component);
         if (component != 'country') {
             document.getElementById(component).value = '';
             document.getElementById(component).disabled = false;
@@ -58,7 +57,6 @@ function fillInAddress() {
         var addressType = place.address_components[i].types[0];
         if (componentForm[addressType]) {
             if (addressType != 'country') {
-                console.log("add type: " + addressType)
                 var val = place.address_components[i][componentForm[addressType]];
                 document.getElementById(addressType).value = val;
             }
@@ -87,14 +85,18 @@ $(document).ready(function () {
     // Set address to profile's address by default, simulate search
     database.ref(`/users/${emailKey}/`).once('value').then(function (snap) {
         let profileObj = snap.val();
+        console.log(profileObj);
         let profileAdd1 = profileObj.address1;
+        let profileAdd1Num = profileAdd1.substring(0, indexOf(" ") - 1);
+        let profileAddRoute = profileAdd1.substring(profileAdd1Num.length, profileAdd1.length);
         //let profileAdd2 = profileObj.address2;
         let profileCity = profileObj.city;
         let profileState = profileObj.state;
         let profileZip = profileObj.zip;
-        let profileAddress = profileAdd1 + ", " + profileCity + "," + profileState + profileZip;
+        let profileAddress = profileAdd1 + ", " + profileCity + "," + profileState + " " + profileZip;
 
-        $(".streetAddress").val(profileAdd1);
+        $(".streetAddressNumber").val(profileAdd1Num);
+        $(".streetAddressRoute").val(profileAddRoute)
         $(".cityAddress").val(profileCity);
         $(".stateAddress").val(profileState);
         $(".zipAddress").val(profileZip);
@@ -103,10 +105,19 @@ $(document).ready(function () {
 
 
     $(".searchMap").on("click", function () {
-        let streetAddress = $(".streetAddress");
-        let cityAddress = $(".cityAddress");
-        let stateAddress = $(".stateAddress");
-        let zipAddress = $(".zipAddress");
+        let streetAddressNum = $(".streetAddressNumber").val();
+        let streetAddressRoute = $(".streetAddressRoute").val();
+        let cityAddress = $(".cityAddress").val();
+        let stateAddress = $(".stateAddress").val();
+        let zipAddress = $(".zipAddress").val();
+
+        if (streetAddressNum === "" || streetAddressRoute === "" || cityAddress === "" || stateAddress === "" || zipAddress === "") {
+            $(".noData").html(`<div class="alert-danger text-center">Please enter a valid US address.</div>`);
+            return;
+        }
+
+        let searchAddress = streetAddressNum + " " + streetAddressRoute + ", " + cityAddress + ", " + stateAddress + " " + zipAddress;
+        console.log(searchAddress);
         var locations = [];
 
         var testAddress = "2515 Catamount St, Bozeman, MT 59715";
@@ -115,20 +126,22 @@ $(document).ready(function () {
         let AddressLookup = testAddress
         let AddressPOST = encodeURIComponent(AddressLookup)
         const searchPoll = address => {
-            $.get(locationsUrl + electionID + "&address=" + AddressPOST/*+"&levels="+govt_level*/).then(response => {
-                let noEarlyVoting="Early voting information is not yet available for this location. You can visit"+"<a href="+"https://docs.google.com/spreadsheets/d/11XD-WNjtNo3QMrGhDsiZH9qZ4N8RYmfpszJOZ_qH1g8/edit#gid=0"+">"+"target="+"_blank"+">Voting Schedule</a>"+ "to see when your location's voting data will be available, and then come back to find your polling place!</a>"
-               if(!(response.earlyVoteSites)){noEarlyVoting}
-               else{
-                // Empty the div so we don't have append multiple if user clicks search >1 time
-                if ($(".locationInfo").length != 0) {
-                    $(".locationInfo").empty();
+            $.get(locationsUrl + electionID + "&address=" + searchAddress/*+"&levels="+govt_level*/).then(response => {
+                console.log(response);
+                if (!(response.earlyVoteSites)) {
+                    $(".noData").html(`<div class="alert-danger">Early voting information is not yet available for this location. You can visit <a href ="https://docs.google.com/spreadsheets/d/11XD-WNjtNo3QMrGhDsiZH9qZ4N8RYmfpszJOZ_qH1g8/edit#gid=0" target="_blank">this page</a> to see when your location's voting data will be available. Please come back and visit us then!</a> </div>`)
                 }
+                else {
+                    // Empty the div so we don't have append multiple if user clicks search >1 time
+                    if ($(".locationInfo").length != 0) {
+                        $(".locationInfo").empty();
+                    }
 
-                if ($(".pollingTimeTable").length != 0) {
-                    $(".pollingTimeTable").empty();
-                }
+                    if ($(".pollingTimeTable").length != 0) {
+                        $(".pollingTimeTable").empty();
+                    }
 
-                $(".pollingTimeTable").append(`<div class="table-wrapper-scroll-y">
+                    $(".pollingTimeTable").append(`<div class="table-wrapper-scroll-y">
                 <table class="table table-bordered table-striped timeTable"<thead>
                 <tr>
                   <th scope="col">Day of Week</th>
@@ -140,34 +153,34 @@ $(document).ready(function () {
               </div>`);
 
 
-                //populate voting dates table
-                for (let i = 0; i < response.earlyVoteSites.length; i++) {
-                    locations.push(response.earlyVoteSites[i].address.line1 + ", " + response.earlyVoteSites[i].address.line2 + ", " + response.earlyVoteSites[i].address.city + ", " + response.earlyVoteSites[i].address.state + ", " + response.earlyVoteSites[i].address.zip);
-                    let splitPollingHours = response.earlyVoteSites[i].pollingHours.split(/\n/g) || [];
+                    //populate voting dates table
+                    for (let i = 0; i < response.earlyVoteSites.length; i++) {
+                        locations.push(response.earlyVoteSites[i].address.line1 + ", " + response.earlyVoteSites[i].address.line2 + ", " + response.earlyVoteSites[i].address.city + ", " + response.earlyVoteSites[i].address.state + ", " + response.earlyVoteSites[i].address.zip);
+                        let splitPollingHours = response.earlyVoteSites[i].pollingHours.split(/\n/g) || [];
 
-                    splitPollingHours.forEach(day => {
-                        let pollingDay = day.substring(0, day.indexOf(","));
-                        pollingDay = fullDayOfWeek(pollingDay);
-                        let pollingDate = day.substring(day.indexOf(",") + 1, day.indexOf(":"));
-                        let pollingHours = day.split(':').pop();
-                        $(".timeTable").append(`<tr><td>${pollingDay}</td><td>${pollingDate}</td><td>${pollingHours}</td></tr>`);
-                    });
+                        splitPollingHours.forEach(day => {
+                            let pollingDay = day.substring(0, day.indexOf(","));
+                            pollingDay = fullDayOfWeek(pollingDay);
+                            let pollingDate = day.substring(day.indexOf(",") + 1, day.indexOf(":"));
+                            let pollingHours = day.split(':').pop();
+                            $(".timeTable").append(`<tr><td>${pollingDay}</td><td>${pollingDate}</td><td>${pollingHours}</td></tr>`);
+                        });
 
 
 
-                    let startDate = response.earlyVoteSites[i].startDate;
-                    startDate = moment(startDate).format("L");
-                    let endDate = response.earlyVoteSites[i].endDate;
-                    endDate = moment(endDate).format("L");
+                        let startDate = response.earlyVoteSites[i].startDate;
+                        startDate = moment(startDate).format("L");
+                        let endDate = response.earlyVoteSites[i].endDate;
+                        endDate = moment(endDate).format("L");
 
-                    $(".locationInfo").append(`<tr>
+                        $(".locationInfo").append(`<tr>
                      <td class="vote-site-name">${response.earlyVoteSites[i].address.locationName}<br>${response.earlyVoteSites[i].address.line1}, ${response.earlyVoteSites[i].address.city}, ${response.earlyVoteSites[i].address.state} ${response.earlyVoteSites[i].address.zip}
                      <br>Polling Location Notes: ${response.earlyVoteSites[i].notes}
                      <br>Date from ${startDate} to ${endDate}
                      </td>`
-                    );
-                }//end populate
-            }
+                        );
+                    }//end populate
+                }
                 //put markers on map
                 locations.forEach(address => {
                     let parsedAddress = urlAddress(address);
